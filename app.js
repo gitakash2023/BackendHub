1f38a5ee0932592a5add7d2c1b37a69d16bc5ac9
Akash <yogibabu32@gmail.com>
2024-01-09 17:32:06 +0530

    hello world

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ require('./coneection/connection');
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
